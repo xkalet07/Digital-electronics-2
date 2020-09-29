e8954c8bd9bd134d34b96cf93dec5b69b5f449cf
xkalet07 <60697133+xkalet07@users.noreply.github.com>
2020-09-29 11:26:22 +0200

    Add files via upload
    [HOME] homework LAB1

--- Labs/01-Tools/led-blink/led-blink/main.c
+++ Labs/01-Tools/led-blink/led-blink/main.c
@@ -11,7 +11,9 @@
 
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN   PB5     // AVR pin where green LED is connected
-#define SHORT_DELAY 1000      // Delay in miliseconds
+#define SHORT_DELAY 300     // Delay in miliseconds
+#define	LONG_DELAY 1000		// 
+#define	END_DELAY 3000		//
 #ifndef F_CPU
 #define F_CPU 16000000      // CPU frequency in Hz required for delay func
 #endif
@@ -30,7 +32,7 @@
  */
 int main(void)
 {
-    // Set pin as output in Data Direction Register
+    /* // Set pin as output in Data Direction Register
     // DDRB = DDRB or 0010 0000
     DDRB = DDRB | (1<<LED_GREEN);											// << je binární shift doleva, | je logický souèet OR 
 
@@ -49,8 +51,65 @@ int main(void)
         PORTB = PORTB ^ (1<<LED_GREEN);										// ^ je XOR
     }
 
-    // Will never reach this
-    return 0;
+    // Will never reach this */
+    PORTB = PORTB & ~(1<<LED_GREEN);		// led off
+	while (1)								// DE2 morse code
+	{
+		// D
+		PORTB = PORTB ^ (1<<LED_GREEN);		// led on
+		_delay_ms(LONG_DELAY);				// _
+		PORTB = PORTB ^ (1<<LED_GREEN);		
+		_delay_ms(SHORT_DELAY);
+		
+		PORTB = PORTB ^ (1<<LED_GREEN);		// .
+		_delay_ms(SHORT_DELAY);				
+		PORTB = PORTB ^ (1<<LED_GREEN);
+		_delay_ms(SHORT_DELAY);
+		
+		PORTB = PORTB ^ (1<<LED_GREEN);		// .
+		_delay_ms(SHORT_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);
+		
+		_delay_ms(LONG_DELAY);
+		
+		// E
+		PORTB = PORTB ^ (1<<LED_GREEN);		// .
+		_delay_ms(SHORT_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);
+		
+		_delay_ms(LONG_DELAY);
+		
+		// 2
+		PORTB = PORTB ^ (1<<LED_GREEN);		// .
+		_delay_ms(SHORT_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);
+		_delay_ms(SHORT_DELAY);
+		
+		PORTB = PORTB ^ (1<<LED_GREEN);		// .
+		_delay_ms(SHORT_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);
+		_delay_ms(SHORT_DELAY);
+		
+		PORTB = PORTB ^ (1<<LED_GREEN);		// _
+		_delay_ms(LONG_DELAY);				
+		PORTB = PORTB ^ (1<<LED_GREEN);
+		_delay_ms(SHORT_DELAY);
+		
+		PORTB = PORTB ^ (1<<LED_GREEN);		// _
+		_delay_ms(LONG_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);
+		_delay_ms(SHORT_DELAY);
+		
+		PORTB = PORTB ^ (1<<LED_GREEN);		// _
+		_delay_ms(LONG_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);
+		_delay_ms(SHORT_DELAY);
+		
+		_delay_ms(END_DELAY);
+	}
+	
+	
+	return 0;
 }
 
 /* Interrupt routines ------------------------------------------------*/
