30a88dfbf15bb55e1b1f870dea0b1ccbf44b2901
xkalet07 <xkalet07@vutbr.cz>
2020-11-23 07:55:43 +0100

    started the project

---
+++ Labs/project/range_meter/range_meter/main.c
@@ -0,0 +1,18 @@
+/*
+ * range_meter.c
+ *
+ * Created: 18.11.2020 12:37:41
+ * Author : Kačkabaskeťačka
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
